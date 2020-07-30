dcf0c040e31629ae3184e3840ee7578da66e12f0
Alejandro Galue <agalue@opennms.org>
2020-07-30 14:38:11 -0400

    Add logic to send a sample event.

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Modal from 'react-bootstrap/Modal';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import * as moment from 'moment';
 
-import { Empty } from './protobuf/oia/model_pb';
+import { Empty, InMemoryEvent } from './protobuf/oia/model_pb';
 import { AlarmLifecycleListenerClient } from "./protobuf/oia/alarms_grpc_web_pb";
+import { EventForwarderClient } from "./protobuf/oia/events_grpc_web_pb";
 
-const client = new AlarmLifecycleListenerClient('http://localhost:8000')
+const alarmsClient = new AlarmLifecycleListenerClient('http://localhost:8000');
+const eventsClient = new EventForwarderClient('http://localhost:8000');
 
 const severityColors = [
   '#ffffff', // Not used
@@ -30,11 +34,11 @@ const handleError = err => {
 
 function App() {
   const [alarms, setAlarms] = React.useState([]);
-  const [showDetail, setShowDetail] = React.useState(false)
+  const [showAlarmDetail, setShowAlarmDetail] = React.useState(false)
   const [selectedAlarm, setSelectedAlarm] = React.useState({})
 
   const loadAlarms = React.useCallback(() => {
-    client.handleAlarmSnapshot(new Empty(), {}, (err, proto) => {
+    alarmsClient.handleAlarmSnapshot(new Empty(), {}, (err, proto) => {
       if (err) {
         handleError(err);
       } else {
@@ -48,7 +52,7 @@ function App() {
   }, []);
 
   const handleNewOrUpdatedAlarm = React.useCallback(() => {
-    const newOrUpdateStream = client.handleNewOrUpdatedAlarm(new Empty(), {});
+    const newOrUpdateStream = alarmsClient.handleNewOrUpdatedAlarm(new Empty(), {});
     newOrUpdateStream.on('error', handleError);
     newOrUpdateStream.on('data', protoAlarm => {
       const alarm = protoAlarm.toObject();
@@ -67,7 +71,7 @@ function App() {
   }, []);
 
   const handleDeletedAlarm = React.useCallback(() => {
-    const deletedStream = client.handleDeletedAlarm(new Empty(), {});
+    const deletedStream = alarmsClient.handleDeletedAlarm(new Empty(), {});
     deletedStream.on('error', handleError);
     deletedStream.on('data', protoAlarm => {
       const alarm = protoAlarm.toObject();
@@ -76,14 +80,22 @@ function App() {
     });
   }, []);
 
-  const handleViewAlarm = alarm => {
+  const handleOpenAlarmModal = alarm => {
     setSelectedAlarm(alarm);
-    setShowDetail(true);
+    setShowAlarmDetail(true);
   };
 
-  const handleClose = () => {
-    setShowDetail(false);
+  const handleCloseAlarmModal = () => {
+    setShowAlarmDetail(false);
   };
+
+  const handleSendEvent = () => {
+    const e = new InMemoryEvent();
+    e.setUei("uei.opennms.org/grpc/reactTest");
+    e.setSource("ReactJS");
+    e.setSeverity(7);
+    eventsClient.sendAsync(e);
+  }
 
   React.useEffect(() => {
     console.log('Initialize handlers.');
@@ -106,7 +118,7 @@ function App() {
           <Card.Text>
             {a.logMessage}
           </Card.Text>
-          <Card.Link href="#" onClick={() => handleViewAlarm(a)}>Details</Card.Link>
+          <Card.Link href="#" onClick={() => handleOpenAlarmModal(a)}>Details</Card.Link>
         </Card.Body>
       </Card>
     </ListGroup.Item>
@@ -117,14 +129,19 @@ function App() {
     <Container fluid>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand>OpenNMS Alarm Feeder</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Form inline>
+            <Button variant="success" onClick={handleSendEvent}>Send Event</Button>
+          </Form>
+        </Nav>
       </Navbar>
-      <Modal show={showDetail} onHide={handleClose}>
+      <Modal show={showAlarmDetail} onHide={handleCloseAlarmModal}>
         <Modal.Header closeButton style={{backgroundColor: severityColors[selectedAlarm.severity]}}>
           <Modal.Title>Alarm Detail (ID={selectedAlarm.id}, Severity={selectedAlarm.severity})</Modal.Title>
         </Modal.Header>
         <Modal.Body>{selectedAlarm.description}</Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleCloseAlarmModal}>
             Close
           </Button>
         </Modal.Footer>
