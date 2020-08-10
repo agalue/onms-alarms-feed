68197b9ab5a099424201aeccd88c9cfe9ef19adb
Alejandro Galue <agalue@opennms.org>
2020-08-10 09:16:03 -0400

    Refactor client to auto re-create streams on server disconnection and using Uber ZAP for logging

--- client-go/alarms/client.go
+++ client-go/alarms/client.go
@@ -4,74 +4,155 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"log"
-	"sync"
+	"io"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/agalue/onms-alarms-feed/protobuf/oia"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
-func displayAlarm(prefix string, alarm *oia.Alarm) {
-	bytes, _ := json.MarshalIndent(alarm, "", "  ")
-	log.Printf("%s:\n%s", prefix, string(bytes))
+var log *zap.SugaredLogger
+
+var client oia.AlarmLifecycleListenerClient
+var updatedStream oia.AlarmLifecycleListener_HandleNewOrUpdatedAlarmClient
+var deletedStream oia.AlarmLifecycleListener_HandleDeletedAlarmClient
+
+func startUpdatedStream() error {
+	var err error
+
+	if updatedStream != nil {
+		updatedStream.CloseSend()
+	}
+
+	updatedStream, err = client.HandleNewOrUpdatedAlarm(context.Background(), &oia.Empty{})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			if updatedStream == nil {
+				break
+			}
+			if alarm, err := updatedStream.Recv(); err == nil {
+				bytes, _ := json.MarshalIndent(alarm, "", "  ")
+				log.Infof("alarm added or updated: %s", string(bytes))
+				continue
+			} else {
+				if err == io.EOF {
+					break
+				}
+				errStatus, _ := status.FromError(err)
+				if errStatus.Code().String() != "Unavailable" {
+					log.Errorf("Cannot receive alarm details: %v", err)
+				}
+			}
+		}
+		log.Warnf("New/Update stream terminated")
+	}()
+
+	return nil
 }
 
-func displayDeletedAlarm(prefix string, alarm *oia.DeleteAlarm) {
-	bytes, _ := json.MarshalIndent(alarm, "", "  ")
-	log.Printf("%s:\n%s", prefix, string(bytes))
+func startDeletedStream() error {
+	var err error
+
+	if deletedStream != nil {
+		deletedStream.CloseSend()
+	}
+
+	deletedStream, err = client.HandleDeletedAlarm(context.Background(), &oia.Empty{})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			if deletedStream == nil {
+				break
+			}
+			if alarm, err := deletedStream.Recv(); err == nil {
+				bytes, _ := json.MarshalIndent(alarm, "", "  ")
+				log.Infof("alarm deleted: %s", string(bytes))
+				continue
+			} else {
+				if err == io.EOF {
+					break
+				}
+				errStatus, _ := status.FromError(err)
+				if errStatus.Code().String() != "Unavailable" {
+					log.Errorf("Cannot receive alarm deleted details: %v", err)
+				}
+			}
+		}
+		log.Warnf("Delete stream terminated")
+	}()
+
+	return nil
 }
 
 func main() {
+	logger, _ := zap.NewDevelopment()
+	log = logger.Sugar()
+
 	bootstrap := flag.String("bootstrap", "127.0.0.1:8991", "gRPC server connection string")
 	flag.Parse()
 
-	conn, err := grpc.Dial(*bootstrap, grpc.WithInsecure())
+	log.Infof("Connecting to %s", *bootstrap)
+	conn, err := grpc.Dial(*bootstrap, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("Error while connecting: %v", err)
 	}
 	defer conn.Close()
-	client := oia.NewAlarmLifecycleListenerClient(conn)
 
-	wg := &sync.WaitGroup{}
-	wg.Add(2)
+	client = oia.NewAlarmLifecycleListenerClient(conn)
 
-	// Start handler for new or updated alarms
+	// Start handler for new/updated alarms
 
-	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-		handler, err := client.HandleNewOrUpdatedAlarm(context.Background(), &oia.Empty{})
-		if err != nil {
-			log.Printf("Error while creating HandleNewOrUpdatedAlarm: %v", err)
-			return
-		}
+	if err := startUpdatedStream(); err != nil {
+		log.Fatalf("Cannot create HandleNewOrUpdatedAlarm: %v", err)
+	}
+
+	go func() {
+		<-updatedStream.Context().Done()
 		for {
-			if alarm, err := handler.Recv(); err == nil {
-				displayAlarm("From HandleNewOrUpdatedAlarm", alarm)
-			} else {
-				log.Printf("Error while getting alarm list: %v", err)
+			if err := startUpdatedStream(); err == nil {
+				log.Warn("New/Update stream restarted")
 				return
 			}
+			time.Sleep(5 * time.Second)
 		}
-	}(wg)
+	}()
 
 	// Start handler for deleted alarms
 
-	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-		handler, err := client.HandleDeletedAlarm(context.Background(), &oia.Empty{})
-		if err != nil {
-			log.Printf("Error while creating HandleDeletedAlarm: %v", err)
-			return
-		}
+	if err := startDeletedStream(); err != nil {
+		log.Fatalf("Cannot create HandleDeletedAlarm: %v", err)
+	}
+
+	go func() {
+		<-deletedStream.Context().Done()
 		for {
-			if alarm, err := handler.Recv(); err == nil {
-				displayDeletedAlarm("From HandleNewOrUpdatedAlarm", alarm)
-			} else {
-				log.Printf("Error while getting alarm list: %v", err)
+			if err := startDeletedStream(); err == nil {
+				log.Warn("Delete stream restarted")
 				return
 			}
+			time.Sleep(5 * time.Second)
 		}
-	}(wg)
+	}()
 
-	wg.Wait()
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Warnf("Terminating")
+	if updatedStream != nil {
+		updatedStream.CloseSend()
+	}
+	if deletedStream != nil {
+		deletedStream.CloseSend()
+	}
 }
