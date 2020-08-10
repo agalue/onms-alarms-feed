a167e0a09d05b4d503943c2ed372dc39bdb1c3cc
Alejandro Galue <agalue@opennms.org>
2020-08-10 09:24:53 -0400

    Refactor client to auto re-create stream on server disconnection and using Uber ZAP for logging

--- client-go/events/client.go
+++ client-go/events/client.go
@@ -4,13 +4,54 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
-	"log"
+	"io"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/agalue/onms-alarms-feed/protobuf/oia"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
+var log *zap.SugaredLogger
+
+var client oia.EventListenerClient
+var handler oia.EventListener_OnEventClient
+
+func startEventListener() error {
+	var err error
+
+	handler, err = client.OnEvent(context.Background(), &oia.EventListenerId{Name: "go-client"})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			if event, err := handler.Recv(); err == nil {
+				bytes, _ := json.MarshalIndent(event, "", "  ")
+				log.Infof("Received event: %s", string(bytes))
+				if err == io.EOF {
+					break
+				}
+				errStatus, _ := status.FromError(err)
+				if errStatus.Code().String() != "Unavailable" {
+					log.Errorf("Cannot receive event: %v", err)
+				}
+			}
+		}
+		log.Warnf("OnEvent stream terminated")
+	}()
+
+	return nil
+}
+
 func main() {
+	logger, _ := zap.NewDevelopment()
+	log = logger.Sugar()
+
 	bootstrap := flag.String("bootstrap", "127.0.0.1:8991", "gRPC server connection string")
 	flag.Parse()
 
@@ -18,20 +59,30 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error while connecting: %v", err)
 	}
-	defer conn.Close()
-	client := oia.NewEventListenerClient(conn)
 
-	handler, err := client.OnEvent(context.Background(), &oia.EventListenerId{Name: "go-client"})
-	if err != nil {
+	client = oia.NewEventListenerClient(conn)
+
+	if err := startEventListener(); err != nil {
 		log.Fatalf("Error while creating OnEvent: %v", err)
 	}
-	for {
-		if event, err := handler.Recv(); err == nil {
-			bytes, _ := json.MarshalIndent(event, "", "  ")
-			log.Println(string(bytes))
-		} else {
-			log.Printf("Error while getting alarm list: %v", err)
-			return
+
+	go func() {
+		<-handler.Context().Done()
+		for {
+			if err := startEventListener(); err == nil {
+				log.Warn("Event listener restarted")
+				return
+			}
+			time.Sleep(5 * time.Second)
 		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+	log.Warnf("Terminating")
+	if handler != nil {
+		handler.CloseSend()
 	}
+	conn.Close()
 }
